600af50ab2d14097a90288c21f890198d077a2cf
Matusael Turski <matusael@protonmail.ch>
2022-10-05 05:30:08 -0300

    Adicionando Estado e Cidade

---
+++ src/main/java/leasutam/Repositories/EstadoRepository.java
@@ -0,0 +1,11 @@
+package leasutam.Repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import leasutam.domain.Estado;
+
+@Repository
+public interface EstadoRepository extends JpaRepository<Estado, Integer> {
+
+}
